9c9702005768ae3e343834a74294fa9afef8e619
David <david87124@gmail.com>
2021-11-09 18:44:46 +0800

    feat: Add DailyReportController

--- routes/dailyReportRouter.js
+++ routes/dailyReportRouter.js
@@ -4,40 +4,10 @@ const router = express.Router('')
 const { dtFormat } = require('../util/date')
 const date = require('date-and-time');
 const { rmNullValue } = require('../util/json')
+const { getDailyReport } = require("../controller/dailyReportController");
 
 // 取得組織內daily report
-router.get('/', authenticationToken, async (req, res, next) => {
-  try {
-    const defaultStartDate = date.format(new Date(2021, 1, 1), "Y-MM-DD")
-    const defaultEndDate = date.format(new Date(), "Y-MM-DD")
-    const { startDate, endDate } = req.query
-    const { organization } = req.auth
-
-    let sql = `
-    SELECT
-      d.id,
-      u.id AS userId,
-      u.name AS userName,
-      d.content,
-      d.date,
-      d.created,
-      d.updated
-    FROM users u
-    JOIN (SELECT * FROM daily_reports WHERE organization_id = ${organization} AND 
-      date >= '${startDate ?? defaultStartDate}' AND
-      date <= '${endDate ?? defaultEndDate}') d
-      ON d.user_id = u.id
-    JOIN (SELECT * FROM user_relations WHERE organization_id = ${organization}) AS r
-      ON r.user_id = u.id
-    ORDER BY d.created DESC`
-    const [reportRows] = await req.db.execute(sql)
-    res.json({
-      results: reportRows
-    })
-  } catch (error) {
-    next(error)
-  }
-})
+router.get('/', authenticationToken, getDailyReport)
 
 // 新增daily report
 router.put('/', authenticationToken, async (req, res, next) => {
