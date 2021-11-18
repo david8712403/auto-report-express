2af66940a15d71fcc300e496d7f25cad081f2456
David Chen <david87124@gmail.com>
2021-11-18 14:19:27 +0800

    Feat: Add system/statistic API - Get visitCount, loginCount, reportCount Close #4

--- service/systemService.js
+++ service/systemService.js
@@ -1,10 +1,19 @@
 const db = require('../db');
 
 const getStatisticData = async () => {
-  const sql = 'SELECT SUM(refreshCount) FROM tokens';
-  const [refreshCountData] = (await db.execute(sql))[0];
-  const visitCount = parseInt(refreshCountData['SUM(refreshCount)']);
-  return { visitCount: visitCount };
+  const sql = 'SELECT SUM(refreshCount), COUNT(value) FROM tokens';
+  const [countData] = (await db.execute(sql))[0];
+  const visitCount = parseInt(countData['SUM(refreshCount)']);
+  const loginCount = parseInt(countData['COUNT(value)']);
+
+  const repostSql = 'SELECT COUNT(content) FROM daily_reports';
+  const [reportData] = (await db.execute(repostSql))[0];
+  const reportCount = parseInt(reportData['COUNT(content)']);
+  return {
+    visitCount: visitCount,
+    loginCount: loginCount,
+    reportCount: reportCount
+  };
 };
 
 module.exports = {
